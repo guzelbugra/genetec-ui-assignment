f8d62fb4689c5e58c5f46a250a727fe03e4bea0d
Bugra <bugraguzel1@gmail.com>
2026-05-26 16:06:14 +0200

    feat: introduce component showcase environment

---
+++ src/showcase/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div style={{ padding: "3rem" }}>
+      <h1>GENETEC UI Showcase</h1>
+      <p>Library elements will be shown here</p>
+    </div>
+  );
+};
+
+export default App;
